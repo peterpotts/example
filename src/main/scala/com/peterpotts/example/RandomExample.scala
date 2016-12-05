5580e0b632993111246acd9eee28bbd67744b9bd
Peter Potts <peter.potts@cointrust.com>
2016-12-04 16:11:07 -0800

    Fix case class.

--- src/main/scala/com/peterpotts/example/RandomExample.scala
+++ src/main/scala/com/peterpotts/example/RandomExample.scala
@@ -8,6 +8,6 @@ object RandomExample extends RandomExample(Random)
 
 object SecureRandomExample extends RandomExample(new SecureRandom)
 
-case class RandomExample(random: Random) extends Example {
+class RandomExample(random: Random) extends Example {
   private[example] def nextLong() = random.nextLong()
 }
