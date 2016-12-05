bc054f3a917e6a77bede8056bce46f49b7881da3
Peter Potts <peter.potts@cointrust.com>
2016-12-04 16:08:36 -0800

    Add secure random.

--- src/main/scala/com/peterpotts/example/RandomExample.scala
+++ src/main/scala/com/peterpotts/example/RandomExample.scala
@@ -1,7 +1,13 @@
 package com.peterpotts.example
 
+import java.security.SecureRandom
+
 import scala.util.Random
 
-object RandomExample extends Example {
-  private[example] def nextLong() = Random.nextLong()
+object RandomExample extends RandomExample(Random)
+
+object SecureRandomExample extends RandomExample(new SecureRandom)
+
+case class RandomExample(random: Random) extends Example {
+  private[example] def nextLong() = random.nextLong()
 }
